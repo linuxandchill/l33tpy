8417876fc0f02ed67069ea8724a46caadff0ad13
linuxandchill <munyontyler@gmail.com>
2020-05-15 19:43:10 -0700

    merge

--- mergesortedarr/index.py
+++ mergesortedarr/index.py
@@ -18,18 +18,4 @@ def merge(nums1, m, nums2, n):
         k -= 1
         n -= 1
 
-    return nums1
-    # nums1 = nums1[0:len(nums1) - m]
-    # for item in nums2:
-    #     nums1.append(item)
-    # print(nums1)
-
-    # for i in range(len(nums1)):
-    #     for j in range(0, len(nums1) - i - 1):
-    #         if nums1[j] > nums1[j+1]:
-    #             temp = nums1[j]
-    #             nums1[j] = nums1[j+1]
-    #             nums1[j+1] = temp
-
-
-print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
+print(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
