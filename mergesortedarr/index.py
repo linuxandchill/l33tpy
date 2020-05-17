e276fcb58598f6574e135b5f8da385f0e6417559
linuxandchill <munyontyler@gmail.com>
2020-05-17 12:34:34 -0700

    merge sorted array

--- mergesortedarr/index.py
+++ mergesortedarr/index.py
@@ -6,17 +6,19 @@ def merge(nums1, m, nums2, n):
     while m >= 0 and n >= 0:
         if nums1[m] > nums2[n]:
             nums1[k] = nums1[m]
-            k-=1
-            m-=1
+            m -= 1
+            k -= 1
         else:
             nums1[k] = nums2[n]
-            k-=1
-            n-=1
+            n -= 1
+            k -= 1
 
     while n >= 0:
         nums1[k] = nums2[n]
-        k-=1
-        n-=1
+        n -= 1
+        k -= 1
+
+    return nums1
 
 
 ## if you need to sort an array,
