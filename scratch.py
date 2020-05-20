53b3c6bbee302c39202703416b762ffb258144f0
linuxandchill <munyontyler@gmail.com>
2020-05-20 13:29:52 -0700

    scratch

--- scratch.py
+++ scratch.py
@@ -1,7 +1,23 @@
 
 def scratch(x):
-    for i in range(x, 0, -1):
-        print(i)
+	for i in range(x, 0, -1):
+		print(i)
 
 
-assert 12 == 13
+# assert 12 == 13
+
+def countdown(n):
+	if n <= 0:             
+		return n
+
+	print(n)
+	n -= 1
+	countdown(n)
+
+def countup(n):
+    if n >= 0:
+        countup(n - 1)
+        print(n)
+
+
+countup(10)
